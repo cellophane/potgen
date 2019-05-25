bd129f87c078051907f53da38691b2df80edb380
joulesy <kahn.jules@gmail.com>
2019-05-25 06:56:31 -0400

    add tube wraparound function

--- vasegen.py
+++ vasegen.py
@@ -8,7 +8,7 @@ import trimesh
 import copy
 import random
 #random.seed(10204)
-#np.random.seed(10204)
+np.random.seed(10204)
 ##we would like to tile an arbitrary tube with n-ominos
 
 ##generate all n-ominos
@@ -24,8 +24,11 @@ def generateOminos(n,current_omino=set(),ominos = set(), depth=0):
         for element in current_omino:
             for x in [-1,0,1]:
                 for y in [a for a in [-1,0,1] if abs(a)+abs(x)==1]:
+                    
                 
                     new_element = (element[0]+x,element[1]+y)
+                    if new_element[0]==-10:
+                        continue
                     if not new_element in current_omino:
                         new_omino = current_omino.copy()
                         new_omino.add(new_element)
@@ -57,6 +60,105 @@ def printOmino(omino):
         print(cStr)
 #function to tile a rectangle
 placeCount = 0
+
+
+def tileTube(size=[4,4],n=2,quantity=0, ominos = set(), grid = [], omino_list = [], omino_sets = set(), tried_set = set(), depth = 0):
+    #if depth = 0, setup board
+    if quantity>0:
+        if len(omino_sets)>=quantity:
+            return
+    if depth == 0:
+        ominos = generateOminos(n)
+        tried_set = set()
+        for i,omino in enumerate(ominos):
+            canPlace = True
+            for tile in omino:
+                if tile[1]<0 or tile[1]>=size[1]:
+                    canPlace = False
+            if canPlace:
+                grid = np.zeros(size,dtype=int)
+                for tile in omino:
+                    grid[tile[0]%size[0]][tile[1]]=1
+                omino_list = [(i,(0,0))]
+                tileTube(size,n,quantity,ominos,grid,omino_list,omino_sets, tried_set, 1)
+        return omino_sets
+    if depth>0:
+
+        empty_neighbors = set()
+        for x in range(size[0]):
+            for y in range(size[1]):
+                if grid[x][y] == 1:         
+                    for neighborX in [-1,0,1]:
+                        for neighborY in [y for y in [-1,0,1] if abs(neighborX)+abs(y)==1]:
+                            checkX  = x+neighborX
+                            checkY = y+neighborY
+                            try:
+                                if grid[checkX%size[0]][checkY] == 0 and checkY>=0:
+                                    empty_neighbors.add((checkX%size[0],checkY))
+                            except:
+                                pass
+        if len(empty_neighbors)>0:
+            tileChoices = [0]*len(empty_neighbors)
+            for neighborIndex,neighbor in enumerate(empty_neighbors):
+                canPlace = False
+                choiceCount  = 0
+                for i,omino in enumerate(ominos):
+                    tempCanPlace = True
+                    for tile in omino:
+                        try:
+                            if tile[1]+neighbor[1]<0:
+                                tempCanPlace = False
+                                break
+                            if grid[(tile[0]+neighbor[0])%size[0]][tile[1]+neighbor[1]] != 0:
+                                tempCanPlace = False
+                                break
+                        except:
+                            tempCanPlace = False
+                            break
+                    if tempCanPlace:
+                        canPlace = True
+                        choiceCount+=1
+                        
+                if canPlace == False:
+                    return
+                tileChoices[neighborIndex]=choiceCount
+                tileChoices = [i for i, v in sorted(enumerate(tileChoices), key=lambda iv: iv[1])]
+            for neighbor in [list(empty_neighbors)[x] for x in tileChoices]:
+                for i,omino in enumerate(ominos):
+                    canPlace = True
+                    for tile in omino:
+                        try:
+                            if tile[1]+neighbor[1]<0:
+                                canPlace = False
+                                break
+                            if grid[(tile[0]+neighbor[0])%size[0]][tile[1]+neighbor[1]] != 0:
+                                canPlace = False
+                                break
+                        except:
+                            canPlace = False
+                            break
+                    if canPlace and not (i,neighbor) in tried_set:
+                        #print("before")
+                        #print(grid)
+                        for tile in omino:
+                            grid[(tile[0]+neighbor[0])%size[0]][tile[1]+neighbor[1]]=1
+                        omino_list.append((i,neighbor))
+                        #debug variable
+                        global placeCount
+                        placeCount+=1
+                        if placeCount%5000 ==0:
+                            print(grid)
+                            print(countContiguousTube(grid,n))
+                        if countContiguousTube(grid,n):
+                            tileTube(size,n,quantity,ominos,grid,omino_list,omino_sets,tried_set, depth+1)
+                        omino_list.pop()
+                        for tile in omino:
+                            grid[(tile[0]+neighbor[0])%size[0]][tile[1]+neighbor[1]]=0
+                return
+        if len(empty_neighbors)==0:
+            plotOminoSet(frozenset(omino_list),size,n)
+            omino_sets.add(frozenset(omino_list))
+
 def tileRectangle(size=[4,4],n=2,quantity=0, ominos = set(), grid = [], omino_list = [], omino_sets = set(), tried_set = set(), depth = 0):
     #if depth = 0, setup board
     if quantity>0:
@@ -197,7 +299,49 @@ def countContiguous(grid,n):
             return False
     return True
         
-                    
+def countContiguousTube(grid,n):
+    patchMembers = set()
+    patchSizes = []
+    
+    size = np.shape(grid)
+    for x in range(size[0]):
+        for y in range(size[1]):
+            if grid[x][y]!=0:
+                continue
+            if not (x,y) in patchMembers:
+                patchMembers.add((x,y))
+                patchSize = 1
+                neighborList = []
+                for xCheck in [-1,0,1]:
+                    for yCheck in [y for y in[-1,0,1] if abs(y)+abs(xCheck)==1]:
+                        xNeighbor = xCheck+x
+                        yNeighbor = yCheck+y
+                        if yNeighbor<0 or yNeighbor>=size[1]:
+                            continue
+                        if grid[xNeighbor%size[0]][yNeighbor] == 0:
+                            neighborList.append((xNeighbor,yNeighbor))
+                toCheck = neighborList.copy()
+                neighborList.append((x,y))
+                while len(toCheck)>0:
+                    check = toCheck.pop()
+                    if grid[check[0]%size[0]][check[1]]==0:
+                        patchSize+=1
+                        patchMembers.add((check[0]%size[0],check[1]))
+                        for xCheck in [-1,0,1]:
+                            for yCheck in [y for y in[-1,0,1] if abs(y)+abs(xCheck)==1]:
+                                xNeighbor = xCheck+check[0]
+                                yNeighbor = yCheck+check[1]
+                                if yNeighbor<0 or yNeighbor>=size[1]:
+                                    continue
+                                if grid[xNeighbor%size[0]][yNeighbor] == 0 and not (xNeighbor%size[0],yNeighbor) in neighborList:
+                                    neighborList.append((xNeighbor%size[0],yNeighbor))
+                                    toCheck.append((xNeighbor%size[0],yNeighbor))
+                                    
+                patchSizes.append(patchSize)
+    for patchSize in patchSizes:
+        if patchSize%n != 0:
+            return False
+    return True                    
                     
             
 import string
@@ -247,7 +391,7 @@ def plotOminoSet(omino_set,size,n):
         color=np.random.rand(3,)
         
         for tile in omino:
-            center = (tile[0]+coord[0],tile[1]+coord[1])
+            center = ((tile[0]+coord[0])%size[0],tile[1]+coord[1])
             rect = Rectangle(center,1,1,color=color)
             patches.append(rect)
             ax.add_artist(rect)
@@ -259,10 +403,10 @@ def plotOminoSet(omino_set,size,n):
     ax.add_collection(p)
     
     plt.show()
-size = [25,25]
+size = [5,5]
 n = 5
-quantity = 1
-omino_sets = tileRectangle(size,n,quantity)
+quantity = 10000
+omino_sets = tileTube(size,n,quantity)
 set_list = list(omino_sets)
 for i in range(0,len(set_list),max(int(len(set_list)/10),1)):
    plotOminoSet(set_list[i],size,n)
